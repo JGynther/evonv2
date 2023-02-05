3fd6a92f110e498bebaab80c09c8e07c48aeaf8c
Joona Gynther <joona@gynther.xyz>
2023-02-05 02:52:30 +0200

    configure terraform s3 backend

--- main.tf
+++ main.tf
@@ -4,11 +4,30 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+  backend "s3" {
+    bucket = "evon-tfstate"
+    key    = "evon-services"
+    region = "eu-north-1"
+  }
 }
 
 provider "aws" {
   region  = "eu-north-1" // Stockholm
   profile = "evon"
+}
+
+
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "evon-tfstate"
+  tags = {
+    service = "terraform"
+  }
+}
+
+resource "aws_s3_bucket_acl" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  acl    = "private"
 }
 
 module "go-link" {
