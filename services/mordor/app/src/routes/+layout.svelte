cfc7a095a2e06baa0b2f8ca42adc3ee5cb635f3f
Joona Gynther <joona@gynther.xyz>
2023-05-21 22:36:32 +0300

    add basic layout

--- services/mordor/app/src/routes/+layout.svelte
+++ services/mordor/app/src/routes/+layout.svelte
@@ -2,4 +2,16 @@
   import "../global.css";
 </script>
 
-<slot />
+<div class="min-h-screen flex flex-col items-center">
+  <header class="border-b w-full mb-10 px-10 py-6">
+    <a href="/" class="text-xl font-bold">Mordor</a>
+  </header>
+
+  <div class="container max-w-screen-md px-5 flex-grow">
+    <slot />
+  </div>
+
+  <footer class="border-t w-full px-10 py-6">
+    For authorized use only. © 2023 Evon Capital Oy Ab
+  </footer>
+</div>
