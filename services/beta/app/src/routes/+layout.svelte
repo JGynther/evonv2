0893225d009459d0c744c42a656854431b273e52
Joona Gynther <joona@gynther.xyz>
2023-07-17 03:32:39 +0300

    add simple layout to beta

--- services/beta/app/src/routes/+layout.svelte
+++ services/beta/app/src/routes/+layout.svelte
@@ -2,4 +2,28 @@
     import "../global.css";
 </script>
 
-<slot />
+<div class="bg-neutral-900 min-h-screen text-white flex flex-col">
+    <header
+        class="sticky top-0 z-10 mb-12 bg-neutral-900 border-b border-neutral-700"
+    >
+        <div
+            class="py-5 px-14 font-serif tracking-widest text-xl flex items-center"
+        >
+            <img
+                src="logo.webp"
+                alt="Evon Capital logo"
+                width={60}
+                class="mr-4"
+            />
+            EVON CAPITAL
+        </div>
+    </header>
+
+    <div class="flex-grow mx-auto px-5">
+        <slot />
+    </div>
+
+    <footer class="mt-12 py-5 px-14 border-t border-neutral-700">
+        © {new Date().getFullYear()} Evon Capital Oy Ab
+    </footer>
+</div>
--- services/beta/app/src/routes/+page.svelte
+++ services/beta/app/src/routes/+page.svelte
@@ -2,16 +2,36 @@
     import { getAllBlogs } from "$lib/blog";
     import { onMount } from "svelte";
 
-    let blogs = [];
+    type Blog = {
+        content: string;
+        title: string;
+        createdAt: string;
+        authors: string[];
+        id: string;
+    };
+
+    let blogs: Blog[] = [];
 
     onMount(async () => {
         blogs = await getAllBlogs();
+        blogs = blogs.sort(
+            (a, b) =>
+                // Possible performance issues with creating new Date objects?
+                new Date(b.createdAt).getTime() -
+                new Date(a.createdAt).getTime()
+        );
     });
 </script>
 
-{#each blogs as blog}
-    <div>
-        <h2 class="text-xl">{blog.title}</h2>
-        <p>{blog.content.slice(0, 100)}</p>
-    </div>
-{/each}
+<div
+    class="bg-neutral-800 flex flex-col p-5 divide-y divide-neutral-700 rounded"
+>
+    {#each blogs as blog}
+        <a
+            href={`/blog/${blog.id}`}
+            class="p-2 bg-neutral-800 hover:bg-neutral-700 transition"
+        >
+            {new Date(blog.createdAt).toLocaleDateString()} - {blog.title}
+        </a>
+    {/each}
+</div>
